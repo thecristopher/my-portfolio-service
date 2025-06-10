76572833898ebaee8ee9ce4293449b3acaca533a
Cristopher Cervantes <isc.cristopher@gmail.com>
2025-06-09 19:04:59 -0700

    Add CORS middleware to allow requests from specified origins

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from data import project_data, skills_data
 from models import Project, Skill
 
 app = FastAPI(title ="Portfolio API", description="API for my portfolio projects", version="1.0.0")
+
+origins = [
+    "http://localhost:5173",  # Local development
+    "https://cristophercervantes.com",  # Production URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/projects", response_model=list[Project])
 def get_projects():
     """
